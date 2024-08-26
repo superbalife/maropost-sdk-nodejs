fbba40c9449bf80118aae57627b84008932dcb1c
QuantumQuin <quinlan@superbalife.com>
2024-08-26 15:45:18 -0700

    Fix esm imports

--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
-import * as Reports from'./src/Reports.cjs';
-import * as Journeys from'./src/Journeys.cjs';
-import * as Contacts from'./src/Contacts.cjs';
-import * as Campaigns from'./src/Campaigns.cjs';
-import * as AbTestCampaigns from'./src/AbTestCampaigns.cjs';
-import * as ProductAndRevenue from'./src/ProductAndRevenue.cjs';
-import * as RelationalTableRows from'./src/RelationalTableRows.cjs';
-import * as TransactionalCampaigns from'./src/TransactionalCampaigns.cjs';
+import Reports from './src/Reports.cjs';
+import Journeys from './src/Journeys.cjs';
+import Contacts from './src/Contacts.cjs';
+import Campaigns from './src/Campaigns.cjs';
+import AbTestCampaigns from './src/AbTestCampaigns.cjs';
+import ProductAndRevenue from './src/ProductAndRevenue.cjs';
+import RelationalTableRows from './src/RelationalTableRows.cjs';
+import TransactionalCampaigns from './src/TransactionalCampaigns.cjs';
 
 const Maropost = {
   Api: {
